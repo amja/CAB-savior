bfcc49204657bec501705bab0b324220be87c7ff
amja <me@amosjackson.com>
2018-04-21 17:32:24 -0400

    Testing

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ function get_term_code() {
 	if (time.getMonth() >= 10 || time.getMonth() < 4) {
 		code = (time.getFullYear()-1).toString() + "20";
 	} else {
+		console.log("winter!!");
 		code = time.getFullYear().toString() + "10";
 	}
 	return code;
